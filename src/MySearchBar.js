8d5df1946bb72a4ada5c3aae8954c7f60d915f63
RATSIMBAZAFY Anjaraniaina <hei.anjaraniaina@gmail.com>
2022-05-18 22:00:20 +0300

    fix: deleted files

--- src/MySearchBar.js
+++
@@ -1,9 +0,0 @@
-export default function MySearchBar(props){
-    return(
-      <div className="input-group">
-              <input className="form-control" type="text" placeholder="Search for..." aria-label="Search for..." aria-describedby="btnNavbarSearch" />
-              <button className="btn btn-primary" id="btnNavbarSearch" type="button"><i className="fas fa-search"></i></button>
-          </div>
-    )
-  }
-  
